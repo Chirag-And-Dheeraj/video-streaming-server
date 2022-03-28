4c212dae7a4903a9fe1b658f77526077e3724496
Chirag Lulla <lullachirag239@gmail.coom>
2022-03-28 21:43:35 +0530

    :card_file_box: feat: Create database connection

---
+++ database/config.go
@@ -0,0 +1,18 @@
+package database
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func Connect() *sql.DB {
+	db, err := sql.Open("sqlite3", "streaming-server.db")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return db
+}
--- main.go
+++ main.go
@@ -2,21 +2,11 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"log"
 	"net/http"
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load()
-
-	if err != nil {
-		fmt.Println("Error loading .env file")
-	}
-
-	port := os.Getenv("PORT")
-	fmt.Println("Server is running on port", port)
-	if err := http.ListenAndServe(":"+port, nil); err != nil {
-		fmt.Println("Error starting server")
-	}
+	fmt.Println("Server is running on port", 8080)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
