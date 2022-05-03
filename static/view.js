7dc7eddef6e30b53a8ef71ac690d3b15e6353f17
dheerajdlalwani <lalwanidheeraj1234@gmail.com>
2022-05-03 20:30:18 +0530

    :lipstick: Add frontend for view page

--- static/view.js
+++ static/view.js
@@ -3,6 +3,40 @@ window.onload = async () => {
   const response = await fetch("http://127.0.0.1:8000/video", {
     method: "GET",
   });
+
   let videos = await response.json();
+
   console.log(videos);
+
+  let videoListSection = document.getElementById("video_list");
+
+  for (let i = 0; i < videos.length; i++) {
+    let videoListRow = document.createElement("section");
+
+    let videoTitle = document.createElement("h2");
+
+    let videoDescription = document.createElement("p");
+
+    let videoLink = document.createElement("a");
+
+    let videoTitleText = document.createTextNode(videos[i].title);
+
+    let videoDescriptionText = document.createTextNode(videos[i].description);
+
+    videoLink.setAttribute("href", "#");
+
+    videoLink.textContent = "Play";
+
+    videoTitle.appendChild(videoTitleText);
+
+    videoDescription.appendChild(videoDescriptionText);
+
+    videoListRow.appendChild(videoTitle);
+
+    videoListRow.appendChild(videoDescription);
+
+    videoListRow.appendChild(videoLink);
+
+    videoListSection.appendChild(videoListRow);
+  }
 };
