349e29d9f1262fbb2b7b29195998f9697dd99171
Chirag Lulla <lullachirag239@gmail.com>
2022-05-05 06:03:33 +0530

    :sparkles: feat: Delete video from server after uploading

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
-@REM cls && rd /s/q video && rd /s/q segments && del database.db && mkdir video && mkdir segments && go run main.go
-cls && go run main.go
+cls && rd /s/q video && rd /s/q segments && del database.db && mkdir video && mkdir segments && go run main.go
+@REM cls && go run main.go
 @REM cls && rd /s/q video && rd /s/q segments && mkdir video && mkdir segments && go run main.go
