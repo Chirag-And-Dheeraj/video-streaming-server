e96e7f4ddaadd75de5111bb761b292310b1c8b01
Dheeraj Lalwani <lalwanidheeraj1234@gmail.com>
2024-08-18 03:33:58 +0530

    config: Also takes SSL_MODE from .env for pg connection

--- database/config.go
+++ database/config.go
@@ -17,9 +17,10 @@ func Connect() *sql.DB {
 	user := os.Getenv("DB_USER")
 	password := os.Getenv("DB_PASSWORD")
 	dbname := os.Getenv("DB_NAME")
+	sslmode := os.Getenv("SSL_MODE")
 
-	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname)
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		host, port, user, password, dbname, sslmode)
 
 	db, err := sql.Open("postgres", psqlInfo)
 
