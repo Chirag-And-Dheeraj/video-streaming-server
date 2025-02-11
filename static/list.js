923f53a5258c05356d6054bcc67ae6eee2e3ddea
Dheeraj Lalwani <lalwanidheeraj1234@gmail.com>
2025-02-12 00:55:43 +0530

    wip

--- static/list.js
+++ static/list.js
@@ -7,7 +7,7 @@ window.onload = async () => {
 
   console.log(videos);
 
-  if(videos.length > 0) {
+  if (videos.length > 0) {
     let videoListSection = document.getElementById("video_list");
 
     for (let i = 0; i < videos.length; i++) {
@@ -18,6 +18,9 @@ window.onload = async () => {
       let videoDescription = document.createElement("p");
       let videoLink = document.createElement("a");
       let deleteLink = document.createElement("a");
+      let videoThumbnail = document.createElement("img");
+
+      videoThumbnail.src = "http://127.0.0.1:8000/static/logo/dekho_logo.svg";
 
       videoLink.setAttribute(
         "href",
@@ -25,48 +28,48 @@ window.onload = async () => {
       );
       videoLink.textContent = "Play";
 
-      deleteLink.setAttribute(
-        "href",
-        "/list"
-      );
-      deleteLink.setAttribute(
-        "data-id",
-        videos[i].id
-      );
+      deleteLink.setAttribute("href", "/list");
+      deleteLink.setAttribute("data-id", videos[i].id);
       deleteLink.textContent = "Delete";
 
       videoTitle.appendChild(videoTitleText);
 
       videoDescription.appendChild(videoDescriptionText);
 
+      videoListRow.classList.add("hunter");
+
+      videoListRow.appendChild(videoThumbnail);
+
       videoListRow.appendChild(videoTitle);
       videoListRow.appendChild(videoDescription);
       videoListRow.appendChild(videoLink);
-      videoListRow.appendChild(deleteLink)
+      videoListRow.appendChild(deleteLink);
 
       videoListSection.appendChild(videoListRow);
 
+      deleteLink.addEventListener("click", async (e) => {
+        e.preventDefault();
+        console.log("Delete Video");
 
-      deleteLink.addEventListener('click', async (e) => {
-        e.preventDefault()
-        console.log("Delete Video")
-      
-        const video_id = deleteLink.dataset.id
-        console.log(video_id)
-        deleteLink.textContent = "Deleting..."
-  
-        const response = await fetch(`${window.ENV.API_URL}/video/${video_id}`, {
-          method: "DELETE"
-        });
-  
-        if(response.status === 202) {
-          deleteLink.textContent = "Deleted"
+        const video_id = deleteLink.dataset.id;
+        console.log(video_id);
+        deleteLink.textContent = "Deleting...";
+
+        const response = await fetch(
+          `${window.ENV.API_URL}/video/${video_id}`,
+          {
+            method: "DELETE",
+          }
+        );
+
+        if (response.status === 202) {
+          deleteLink.textContent = "Deleted";
         } else {
-          deleteLink.textContent = "Error while deleting"
+          deleteLink.textContent = "Error while deleting";
         }
-  
-        location.reload()
-      })
+
+        location.reload();
+      });
     }
   }
 };
